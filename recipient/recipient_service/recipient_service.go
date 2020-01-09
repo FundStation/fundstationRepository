ceee079366659a0fd7c9d7af399b7458de33eaf1
Adey Amare <56384397+AdeyAmare@users.noreply.github.com>
2020-01-09 20:30:53 +0300

    create recipient_service.go in recipient_service

---
+++ recipient/recipient_service/recipient_service.go
@@ -0,0 +1,47 @@
+package recipient_service
+
+import (
+	"github.com/FundStation/models"
+	"github.com/FundStation/recipient"
+)
+
+type RecipientService struct {
+	rRepo recipient.RecipientRepository
+}
+
+func NewRecipientService(reciRepo recipient.RecipientRepository) *RecipientService {
+	return &RecipientService{rRepo: reciRepo}
+}
+
+func (rs *RecipientService) SignupRecipient(recipient models.Recipient) error {
+
+	err := rs.rRepo.InsertRecipient(recipient)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (rs *RecipientService) ViewAllRecipient() (recipients []models.Recipient, err error) {
+
+	recipients, err = rs.rRepo.SelectAllRecipient()
+
+	if err != nil {
+		return recipients, err
+	}
+
+	return recipients, nil
+}
+
+func (rs *RecipientService) LoginRecipient(recipient models.Recipient) error {
+
+	err := rs.rRepo.SelectRecipient(recipient)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
