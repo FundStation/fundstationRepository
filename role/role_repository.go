17620c7cc0bd7a58f03a7232b78856890fd00652
Kidist Kebede <56719359+KidistKebede@users.noreply.github.com>
2020-01-12 22:58:50 +0200

    role repository

---
+++ role/role_repository.go
@@ -0,0 +1,20 @@
+package role
+
+import "github.com/FundStation2/models"
+
+type RoleRepository interface {
+	Roles() ([]models.Role, error)
+	Role(id uint) (*models.Role, error)
+	RoleByName(name string) (*models.Role, error)
+	UpdateRole(role *models.Role) ( error)
+	DeleteRole(id uint) (error)
+	StoreRole(role *models.Role) (error)
+	DonorRoles(donor *models.Donor) (models.Role, error)
+	RecipientRoles(donor *models.Recipient) (models.Role, error)
+}
+
+type SessionRepository interface {
+	Session(sessionID string) (*models.Session, error)
+	StoreSession(session *models.Session) (*models.Session, error)
+	DeleteSession(sessionID string) (*models.Session, error)
+}
