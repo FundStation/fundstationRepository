ee02dd1bbba8699c10151ae87bc6df81b568766a
hannahKibret <56648297+hannahKibret@users.noreply.github.com>
2020-01-13 00:10:31 +0300

    update recipentinfo_repository.go

--- recipientInfo/recipientInfo_repository.go
+++ recipientInfo/recipientInfo_repository.go
@@ -6,4 +6,6 @@ type RecipientInfoRepository interface {
 	InsertRecipientInfo(recipientInfo models.RecipientInfo) error
 	SelectRecipientInfo(int) (models.RecipientInfo, error)
 	UpdateRecipientInfo(int) error
+	AccountExistsInfo(account string) bool
+	SelectApproved() (rinfo []models.RecipientInfo,err error)
 }
