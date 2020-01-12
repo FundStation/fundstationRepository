57e44892d5dcfe621cc635294b893f45a41a2455
Adey Amare <56384397+AdeyAmare@users.noreply.github.com>
2020-01-12 23:26:03 +0200

    Update recipient_service.go

--- recipient/recipient_service.go
+++ recipient/recipient_service.go
@@ -3,7 +3,12 @@ package recipient
 import "github.com/FundStation/models"
 
 type RecipientService interface {
-	SignupRecipient(recipient *models.Recipient) error
+	SignupRecipient(recipient *models.Recipient)(*models.Recipient,error)
 	LoginRecipient(recipient models.Recipient) error
 	ViewAllRecipient() ([]models.Recipient, error)
+	RecipientByUsername(username string)(*models.Recipient,error)
+	UsernameExists(username string) bool
+	EmailExists(email string) bool
+	PhoneExists(phone string) bool
+
 }
