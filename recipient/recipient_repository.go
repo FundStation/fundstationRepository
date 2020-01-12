c7c4f53e7d3a1703ecd94b4739c5f9080e3866c1
Adey Amare <56384397+AdeyAmare@users.noreply.github.com>
2020-01-12 23:25:14 +0200

    Update recipient_repository.go

--- recipient/recipient_repository.go
+++ recipient/recipient_repository.go
@@ -1,9 +1,15 @@
 package recipient
 
-import "github.com/FundStation/models"
+import (
+	"github.com/FundStation/models"
+)
 
 type RecipientRepository interface {
-	InsertRecipient(recipient *models.Recipient) error
+	InsertRecipient(recipient *models.Recipient)(*models.Recipient, error)
 	SelectRecipient(recipient models.Recipient) error
 	SelectAllRecipient() ([]models.Recipient, error)
+	RecipientByUsername(username string)(*models.Recipient,error)
+	UsernameExists(username string) bool
+	EmailExists(email string) bool
+	PhoneExists(phone string) bool
 }
