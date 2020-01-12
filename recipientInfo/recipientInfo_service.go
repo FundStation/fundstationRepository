5cb53e2fa69db542fca43491f7c1b6843fce1d0f
hannahKibret <56648297+hannahKibret@users.noreply.github.com>
2020-01-13 00:11:30 +0300

    update recipientinfo_service.go

--- recipientInfo/recipientInfo_service.go
+++ recipientInfo/recipientInfo_service.go
@@ -2,9 +2,10 @@ package recipientInfo
 
 import "github.com/FundStation/models"
 
-// CategoryRepository specifies menu category related database operations
 type RecipientInfoService interface {
 	CreateRecipientInfo(recipientInfo models.RecipientInfo) error
 	ViewSpecificRecipientInfo(int) (models.RecipientInfo, error)
 	ApproveRecipientInfo(int) error
+	AccountExistsInfo(account string) bool
+	SelectApproved() (rinfo []models.RecipientInfo,err error)
 }
