6f89b09652fbc2478d3e20c81c1d3117ae13d297
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 02:17:20 +0300

    Update donor_repository

--- donor/donor_repository.go
+++ donor/donor_repository.go
@@ -2,8 +2,16 @@ package donor
 
 import "github.com/FundStation/models"
 
+
 type DonorRepository interface {
-	InsertDonor(donor *models.Donor) error
+	InsertDonor(donor *models.Donor) (*models.Donor,error)
 	SelectDonor(donor models.Donor) error
 	SelectAllDonor() ([]models.Donor, error)
+	DonorByUsername(username string)(*models.Donor,error)
+	DonorById(id int) (*models.Donor,error)
+	UpdateDonorById(donor *models.Donor) (error)
+	DeleteDonorById(donor *models.Donor) (error)
+	UsernameExists(username string) bool
+	EmailExists(email string) bool
+	PhoneExists(phone string) bool
 }
