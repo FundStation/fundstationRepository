2b80cf6d149ba7dfda6fd1689b44cdcacff6c464
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 02:23:20 +0300

    Update donor_service

--- donor/donor_service/donor_service.go
+++ donor/donor_service/donor_service.go
@@ -1,27 +1,30 @@
 package donor_service
 
 import (
+	"fmt"
 	"github.com/FundStation/donor"
 	"github.com/FundStation/models"
 )
+
 
 type DonorService struct {
 	dRepo donor.DonorRepository
 }
 
+
 func NewDonorService(donorRepo donor.DonorRepository) *DonorService {
 	return &DonorService{dRepo: donorRepo}
 }
 
-func (ds *DonorService) SignupDonor(donor *models.Donor) error {
+func (ds *DonorService) SignupDonor(donor *models.Donor) (*models.Donor,error) {
 
-	err := ds.dRepo.InsertDonor(donor)
+	don,err := ds.dRepo.InsertDonor(donor)
 
 	if err != nil {
-		return err
+		return don,err
 	}
 
-	return nil
+	return don,nil
 }
 func (ds *DonorService) LoginDonor(donor models.Donor) error {
 
@@ -33,6 +36,7 @@ func (ds *DonorService) LoginDonor(donor models.Donor) error {
 
 	return nil
 }
+
 func (ds *DonorService) ViewAllDonor() (donors []models.Donor, err error) {
 
 	donors, err = ds.dRepo.SelectAllDonor()
@@ -43,3 +47,54 @@ func (ds *DonorService) ViewAllDonor() (donors []models.Donor, err error) {
 
 	return donors, nil
 }
+
+func (ds *DonorService) PhoneExists(phone string) bool {
+
+	isPhone := ds.dRepo.PhoneExists(phone)
+	fmt.Println(isPhone)
+	return isPhone
+
+
+}
+func (ds *DonorService) UsernameExists(username string) bool {
+	isUser:= ds.dRepo.UsernameExists(username)
+	return isUser
+}
+
+
+func (ds *DonorService) EmailExists(email string) bool {
+	isEmail:= ds.dRepo.EmailExists(email)
+	return isEmail
+}
+
+func (ds *DonorService) DonorByUsername(username string) (*models.Donor,error) {
+	donor,err:=ds.dRepo.DonorByUsername(username)
+	if err != nil{
+		return donor,err
+	}
+	return donor,nil
+}
+func (ds *DonorService) DonorById(id int) (*models.Donor,error) {
+	donor,err:=ds.dRepo.DonorById(id)
+	if err != nil{
+		return donor,err
+	}
+	return donor,nil
+}
+
+func (ds *DonorService) UpdateDonorById(donor *models.Donor) (error) {
+	err:=ds.dRepo.UpdateDonorById(donor)
+	if err != nil{
+		return err
+	}
+	return nil
+}
+func (ds *DonorService) DeleteDonorById(donor *models.Donor) (error) {
+	err:=ds.dRepo.DeleteDonorById(donor)
+	if err != nil{
+		return err
+	}
+	return nil
+}
+
+
