160fd8fa7f0ed5f6097d044d91d6371a7c3d2d99
hannahKibret <56648297+hannahKibret@users.noreply.github.com>
2020-01-29 00:11:26 +0300

    update psql admin.go file

--- admin/admin_repository/psql_admin.go
+++ admin/admin_repository/psql_admin.go
@@ -2,8 +2,9 @@ package admin_repository
 
 import (
 	"database/sql"
-	_ "github.com/lib/go-sql-driver/mysql"
+	//_ "github.com/lib/go-sql-driver/mysql"
 	_ "github.com/lib/pq"
+	"github.com/FundStation2/models"
 )
 
 type PsqlAdminRepository struct {
@@ -12,4 +13,22 @@ type PsqlAdminRepository struct {
 
 func NewPsqlAdminRepository(Conn *sql.DB) *PsqlAdminRepository {
 	return &PsqlAdminRepository{conn: Conn}
+}
+func (pr *PsqlAdminRepository) SelectAdmin(user string) (*models.Admin,error) {
+	admin1 :=&models.Admin{}
+	querystmt, err := pr.conn.Prepare("SELECT * FROM admins WHERE username=$1 ")
+
+	if err != nil {
+		return admin1,err
+	}
+
+
+	err = querystmt.QueryRow(user).Scan(&admin1.Username, &admin1.Password,&admin1.RoleID)
+
+
+	if err != nil {
+		return admin1,err
+	}
+	return admin1,nil
+
 }
