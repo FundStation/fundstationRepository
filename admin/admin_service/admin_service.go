36aa1fc416dc77acb244aae1be63b27e84951d35
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 02:08:21 +0300

    Update admin_service

--- admin/admin_service/admin_service.go
+++ admin/admin_service/admin_service.go
@@ -2,14 +2,25 @@ package admin_service
 
 import (
 	"github.com/FundStation/admin"
+	"github.com/FundStation/models"
 )
 
-// CategoryService implements menu.CategoryService interface
+
 type AdminService struct {
 	aRepo admin.AdminRepository
 }
 
-// NewCategoryService will create new CategoryService object
+
 func NewAdminService(adRepo admin.AdminRepository) *AdminService {
 	return &AdminService{aRepo: adRepo}
+}
+func (as *AdminService) LoginAdmin(user string) (*models.Admin,error) {
+
+	adm,err := as.aRepo.SelectAdmin(user)
+
+	if err != nil {
+		return adm, err
+	}
+
+	return adm,nil
 }
