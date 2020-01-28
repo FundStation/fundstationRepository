49c12cf442c25f77da62c196f54cd7a94ebe5b18
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 02:20:28 +0300

    Update psql_donor

--- donor/donor_repository/psql_donor.go
+++ donor/donor_repository/psql_donor.go
@@ -3,27 +3,30 @@ package donor_repository
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 
 	//"errors"
 
 	"github.com/FundStation/models"
 )
 
+
 type PsqlDonorRepository struct {
 	conn *sql.DB
 }
+
 
 func NewPsqlDonorRepository(Conn *sql.DB) *PsqlDonorRepository {
 	return &PsqlDonorRepository{conn: Conn}
 }
 
-func (pr *PsqlDonorRepository) InsertDonor(d *models.Donor) error {
+func (pr *PsqlDonorRepository) InsertDonor(d *models.Donor) (*models.Donor,error) {
 
-	err := pr.conn.QueryRow("INSERT INTO donor(firstname,lastname,address,username,password,phone,email) VALUES($1, $2, $3, $4, $5, $6, $7) returning id", d.FirstName, d.LastName, d.Address, d.Username, d.Password, d.PhoneNumber, d.EmailAddress).Scan(&d.DonorNo)
+	_, err := pr.conn.Exec("INSERT INTO donor(firstname,lastname,address,occupation,username,password,phone,email,role_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8,$9)", d.FirstName, d.LastName, d.Address, d.Occupation, d.Username, d.Password, d.PhoneNumber, d.EmailAddress,d.RoleID)
 	if err != nil {
-		return err
+		return d,err
 	}
-	return nil
+	return d,nil
 }
 
 func (pr *PsqlDonorRepository) SelectDonor(d models.Donor) error {
@@ -45,21 +48,114 @@ func (pr *PsqlDonorRepository) SelectDonor(d models.Donor) error {
 	if err != nil {
 		return err
 	}
-	
+
+	//_, err := pr.conn.Exec("SELECT username,password FROM recipient WHERE username=$1, password=$2", r.Username, r.Password)
 	return nil
 }
+
 func (pr *PsqlDonorRepository) SelectAllDonor() (donors []models.Donor, err error) {
-	selRec, err := pr.conn.Query("SELECT id ,firstname, lastname, address,occupation,phone,email FROM donor")
+	selRec, err := pr.conn.Query("SELECT * FROM donor")
 	if err != nil {
+		fmt.Println(err)
 		return donors, errors.New("something")
+
 	}
+
 	donor := models.Donor{}
 	for selRec.Next() {
-		err := selRec.Scan(&donor.DonorNo, &donor.FirstName, &donor.LastName, &donor.Address, &donor.Occupation, &donor.PhoneNumber, &donor.EmailAddress)
+		err := selRec.Scan(&donor.FirstName, &donor.LastName, &donor.Address, &donor.Occupation,&donor.Username,&donor.Password, &donor.PhoneNumber, &donor.EmailAddress,&donor.DonorNo,&donor.RoleID)
 		if err != nil {
+			fmt.Println("err",err)
 			return donors, errors.New("Couldnot")
 		}
 		donors = append(donors, donor)
+
 	}
+
 	return donors, nil
+}
+
+func (pr *PsqlDonorRepository) PhoneExists(phone string) bool {
+	var name string
+	err := pr.conn.QueryRow("SELECT firstname FROM donor WHERE phone=$1",phone).Scan(&name)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+func (pr *PsqlDonorRepository) UsernameExists(username string) bool {
+	var name string
+	err := pr.conn.QueryRow("SELECT firstname FROM donor WHERE username=$1",username).Scan(&name)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+// EmailExists check if a given email is found
+func (pr *PsqlDonorRepository) EmailExists(email string) bool {
+	var name string
+	err := pr.conn.QueryRow("SELECT firstname FROM donor WHERE email=$1", email).Scan(&name)
+	if err != nil {
+		return false
+	}
+
+	return true
+}
+
+func (pr *PsqlDonorRepository) DonorByUsername(username string) (*models.Donor,error) {
+	donor:=&models.Donor{}
+	querystmt, err := pr.conn.Prepare("SELECT * FROM donor WHERE username=$1")
+
+	if err != nil {
+		return donor,err
+	}
+	err = querystmt.QueryRow(username).Scan(&donor.FirstName, &donor.LastName, &donor.Address, &donor.Occupation,&donor.Username,&donor.Password, &donor.PhoneNumber, &donor.EmailAddress,&donor.DonorNo,&donor.RoleID)
+	if err != nil{
+		return donor,err
+	}
+
+	return donor,nil
+}
+func (pr *PsqlDonorRepository) DonorById(id int) (*models.Donor,error) {
+	donor:=&models.Donor{}
+	querystmt, err := pr.conn.Prepare("SELECT * FROM donor WHERE id=$1")
+
+	if err != nil {
+		fmt.Println(err)
+		return donor,err
+	}
+	err = querystmt.QueryRow(id).Scan(&donor.FirstName, &donor.LastName, &donor.Address, &donor.Occupation,&donor.Username,&donor.Password, &donor.PhoneNumber, &donor.EmailAddress,&donor.DonorNo,&donor.RoleID)
+	if err != nil{
+		fmt.Println("error",err)
+	}
+	fmt.Println("donr",donor)
+
+	return donor,nil
+}
+func (pr *PsqlDonorRepository) UpdateDonorById(donor *models.Donor) (error) {
+
+	_, err := pr.conn.Exec("UPDATE donor set lastname = $2 where id=$1",donor.DonorNo,donor.LastName)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+
+	return nil
+}
+func (pr *PsqlDonorRepository) DeleteDonorById(donor *models.Donor) (error) {
+
+	_, err := pr.conn.Exec("DELETE FROM donor where id=$1",donor.DonorNo)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+
+	return nil
 }
