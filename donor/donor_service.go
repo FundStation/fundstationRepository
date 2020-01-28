39f987b6f640ef59af20a5768ddc8752e45cd473
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 02:15:00 +0300

    Update donor_service

--- donor/donor_service.go
+++ donor/donor_service.go
@@ -3,7 +3,14 @@ package donor
 import "github.com/FundStation/models"
 
 type DonorService interface {
-	SignupDonor(donor *models.Donor) error
+	SignupDonor(donor *models.Donor) (*models.Donor,error)
 	LoginDonor(donor models.Donor) error
 	ViewAllDonor() ([]models.Donor, error)
+	DonorByUsername(username string)(*models.Donor,error)
+	DonorById(id int) (*models.Donor,error)
+	UpdateDonorById(donor *models.Donor) (error)
+	DeleteDonorById(donor *models.Donor) (error)
+	UsernameExists(username string) bool
+	EmailExists(email string) bool
+	PhoneExists(phone string) bool
 }
