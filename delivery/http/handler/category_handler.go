e0d30aa804fa350f1c8841f7987aa70b2a3af5c4
hannahKibret <56648297+hannahKibret@users.noreply.github.com>
2020-01-29 02:23:56 +0300

    update category handler

--- delivery/http/handler/category_handler.go
+++ delivery/http/handler/category_handler.go
@@ -1,20 +1,28 @@
 package handler
 
 import (
+	"database/sql"
 	"fmt"
-	"github.com/FundStation/category"
+	"github.com/FundStation2/bankAct/bank_repository"
+	"github.com/FundStation2/bankAct/bank_service"
+	"github.com/FundStation2/category"
+	"github.com/FundStation2/form"
 	"html/template"
 	"net/http"
+	"net/url"
 )
+
+
 
 type CategoryHandler struct {
 	tmpl           *template.Template
 	categoryService    category.CategoryService
-	csrfSignKey    []byte
+
 
 }
-func NewCategoryHandler(t *template.Template, cs category.CategoryService,csKey []byte) *CategoryHandler {
-	return &CategoryHandler{tmpl: t, categoryService: cs,csrfSignKey:csKey}
+
+func NewCategoryHandler(t *template.Template, cs category.CategoryService) *CategoryHandler {
+	return &CategoryHandler{tmpl: t, categoryService: cs,}
 }
 
 func (cat *CategoryHandler) SelectMedicalCategory(w http.ResponseWriter, r *http.Request) {
@@ -23,7 +31,7 @@ func (cat *CategoryHandler) SelectMedicalCategory(w http.ResponseWriter, r *http
 	if err != nil{
 		panic(err)
 	}
-	tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
 }
 func (cat *CategoryHandler) SelectWomenCategory(w http.ResponseWriter, r *http.Request) {
 
@@ -31,7 +39,7 @@ func (cat *CategoryHandler) SelectWomenCategory(w http.ResponseWriter, r *http.R
 	if err != nil{
 		panic(err)
 	}
-	tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
 }
 func (cat *CategoryHandler) SelectEducationalCategory(w http.ResponseWriter, r *http.Request) {
 
@@ -39,7 +47,7 @@ func (cat *CategoryHandler) SelectEducationalCategory(w http.ResponseWriter, r *
 	if err != nil{
 		panic(err)
 	}
-	tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
 }
 func (cat *CategoryHandler) SelectOrphanageCategory(w http.ResponseWriter, r *http.Request) {
 
@@ -47,7 +55,7 @@ func (cat *CategoryHandler) SelectOrphanageCategory(w http.ResponseWriter, r *ht
 	if err != nil{
 		panic(err)
 	}
-	tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
 }
 
 func (cat *CategoryHandler) SelectReligiousCategory(w http.ResponseWriter, r *http.Request) {
@@ -56,7 +64,7 @@ func (cat *CategoryHandler) SelectReligiousCategory(w http.ResponseWriter, r *ht
 	if err != nil{
 		panic(err)
 	}
-	tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
 }
 
 func (cat *CategoryHandler) SelectOtherCategory(w http.ResponseWriter, r *http.Request) {
@@ -65,17 +73,84 @@ func (cat *CategoryHandler) SelectOtherCategory(w http.ResponseWriter, r *http.R
 	if err != nil{
 		panic(err)
 	}
-	tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"listCategory.layout",cato)
 }
+var bankAct string
 func (cat *CategoryHandler) SelectSpecificCategory(w http.ResponseWriter,r *http.Request){
 	name:=r.FormValue("catName")
-	fmt.Println(name)
 	cato,err:=cat.categoryService.ViewSpecificCategory(name)
 	if err != nil{
 		fmt.Println(err)
 	}
-	tmpl.ExecuteTemplate(w,"specificEntity.layout",cato)
+	cat.tmpl.ExecuteTemplate(w,"specificEntity.layout",cato)
 }
+
+func (cat *CategoryHandler)Donation(w http.ResponseWriter,r *http.Request){
+
+	cat.tmpl.ExecuteTemplate(w,"donation.layout",bankAcnt)
+}
+
+func (cat *CategoryHandler)DonateMoney(w http.ResponseWriter,r *http.Request) {
+
+
+	dbconn, err := sql.Open("postgres", "postgres://postgres:password@localhost/funddb?sslmode=disable")
+
+	if err != nil {
+		panic(err)
+	}
+	defer dbconn.Close()
+	if r.Method == http.MethodGet {
+		newDonForm := struct {
+			Values  url.Values
+			VErrors form.ValidationErrors
+		}{
+			Values:  nil,
+			VErrors: nil,
+		}
+		cat.tmpl.ExecuteTemplate(w, "donation.layout", newDonForm)
+	}
+	bankRepo := bank_repository.NewPsqlBankRepository(dbconn)
+	bankServ := bank_service.NewBankService(bankRepo)
+	bch := NewBankAccountHandler(bankServ)
+
+		err = r.ParseForm()
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+
+		newDonForm := form.Input{Values: r.PostForm, VErrors: form.ValidationErrors{}}
+		fmt.Println("Value", newDonForm.Values)
+		newDonForm.Required("donorAccount", "amount")
+		fmt.Println("newDonform", newDonForm)
+		newDonForm.ExactLength("donorAccount", 13)
+	//bch.transferMoney(w, r)
+		if !newDonForm.Valid() {
+			cat.tmpl.ExecuteTemplate(w, "donation.layout", newDonForm)
+			fmt.Println("flifla")
+
+			return
+		}
+		aExists := bch.bankAccountService.AccountExists(r.FormValue("donorAccount"))
+		fmt.Println("aExixts", aExists)
+		if aExists {
+			newDonForm.VErrors.Add("donorAccount", "The account number you entered doesn't exist")
+			cat.tmpl.ExecuteTemplate(w, "donation.layout", newDonForm)
+
+			return
+		}
+
+		//if err != nil {
+		//	cat.tmpl.ExecuteTemplate(w, "moneyTransfer.html", err)
+		//}
+		//tmpl.ExecuteTemplate(w, "moneyTransfer.html", nil)
+
+	bch.transferMoney(w, r)
+	//cat.tmpl.ExecuteTemplate(w,"donation.layout",bankAcnt)
+
+	http.Redirect(w, r, "/home", http.StatusSeeOther)
+}
+//}
 
 
 
