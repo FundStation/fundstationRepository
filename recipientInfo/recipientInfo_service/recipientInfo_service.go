61791de6815dcf8571ddb3d38bffe4397775278a
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 02:40:43 +0300

    Update recipient_info

--- recipientInfo/recipientInfo_service/recipientInfo_service.go
+++ recipientInfo/recipientInfo_service/recipientInfo_service.go
@@ -1,6 +1,7 @@
 package recipientInfo_service
 
 import (
+	"fmt"
 	"github.com/FundStation/models"
 	"github.com/FundStation/recipientInfo"
 )
@@ -29,6 +30,58 @@ func (ris *RecipientInfoService) ViewSpecificRecipientInfo(id int) (recipientInf
 	recipientInfo, err = ris.rInfoRepo.SelectRecipientInfo(id)
 
 	if err != nil {
+		return recipientInfo, err
+	}
+
+	return recipientInfo, nil
+}
+func (ris *RecipientInfoService) ApproveRecipientInfo(id int) (err error) {
+
+	err = ris.rInfoRepo.UpdateRecipientInfo(id)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+func (ris *RecipientInfoService) AccountExistsInfo(account string) bool {
+	isAccountInfo:= ris.rInfoRepo.AccountExistsInfo(account)
+	return isAccountInfo
+}
+
+func (ris *RecipientInfoService) SelectApproved() (dInfo []models.DonationInfo,err error){
+	dInfo,err=ris.rInfoRepo.SelectApproved()
+	if(err != nil){
+		return dInfo,err
+
+	}
+	return dInfo,nil
+
+}
+func (ris *RecipientInfoService) SelectIndividualById(id int) (dInfo models.DonationInfo,err error){
+	dInfo,err=ris.rInfoRepo.SelectIndividualById(id)
+	if(err != nil){
+		return dInfo,err
+
+	}
+	return dInfo,nil
+
+}
+
+func (ris  *RecipientInfoService) DeleteRecipientInfoById(id int) (error) {
+
+	err := ris.rInfoRepo.DeleteRecipientInfoById(id)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+
+	return nil
+}
+
 		return recipientInfo, err
 	}
 
