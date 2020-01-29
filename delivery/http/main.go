50b31060148f43ad4061a3e85e5876889d26e197
Hanan-Mohsin <45768074+Hanan-Mohsin@users.noreply.github.com>
2020-01-29 03:04:39 +0300

    Update main.go

--- delivery/http/main.go
+++ delivery/http/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"github.com/FundStation/admin/admin_repository"
+	"github.com/FundStation/admin/admin_service"
 	"github.com/FundStation/category/category_repository"
 	"github.com/FundStation/category/category_service"
+	"github.com/FundStation/delivery/http/REST_API"
 	"github.com/FundStation/donor/donor_repository"
 	"github.com/FundStation/donor/donor_service"
 	"github.com/FundStation/models"
@@ -14,9 +17,6 @@ import (
 	"github.com/FundStation/role/role_repository"
 	"github.com/FundStation/role/role_service"
 	"github.com/FundStation/tokens"
-	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"github.com/julienschmidt/httprouter"
-	_ "github.com/lib/pq"
 	"net/http"
 	"time"
 
@@ -24,10 +24,10 @@ import (
 	"html/template"
 )
 
-var tmpl = template.Must(template.ParseGlob("web/template/*"))
+
 
 func main() {
-
+	var tmpl = template.Must(template.ParseGlob("web/template/*"))
 
 	csrfSignKey := []byte(tokens.GenerateRandomID(32))
 
@@ -54,51 +54,89 @@ func main() {
 	catRepo := category_repository.NewPsqlCategoryRepository(dbcon)
 	catServ := category_service.NewCategoryService(catRepo)
 
+	admRepo := admin_repository.NewPsqlAdminRepository(dbcon)
+	admServ := admin_service.NewAdminService(admRepo)
+
+
 
 	roleRepo := role_repository.NewRoleRepository(dbcon)
 	roleServ := role_service.NewRoleService(roleRepo)
 
+
 	sess := configSess()
+	hch := handler.NewHomeHandler(tmpl)
 	dch:= handler.NewDonorHandler(tmpl, donServ, sessionSrv, roleServ, sess, csrfSignKey)
 	rch:= handler.NewRecipientHandler(tmpl, recServ, sessionSrv, roleServ, sess, csrfSignKey)
 	rich:= handler.NewRecipientInfoHandler(tmpl, recInfoServ, csrfSignKey)
-	ch := handler.NewCategoryHandler(tmpl,catServ,csrfSignKey)
+	ch := handler.NewCategoryHandler(tmpl,catServ)
+	ah := handler.NewAdminHandler(tmpl,admServ,sessionSrv,roleServ,sess,csrfSignKey)
 
+
+	dah:= REST_API.NewDonorApiHandler(donServ)
+	rah:= REST_API.NewRecipientApiHandler(recServ)
+	riah:=REST_API.NewRecipientInfoApiHandler(recInfoServ)
 
 	fs := http.FileServer(http.Dir("web/assets"))
 	http.Handle("/assets/", http.StripPrefix("/assets/", fs))
-	http.Handle("/doners", dch.Authenticated(dch.Authorized(http.HandlerFunc(dch.Doners))))
-	http.HandleFunc("/donor/signup", dch.DonorSignup)
+	http.Handle("/donors", dch.Authenticated(dch.Authorized(http.HandlerFunc(dch.Doners))))
+	http.HandleFunc("/donor", dch.Donor)
+	http.HandleFunc("/donor/signup",dch.DonorSignup)
+	//http.Handle("/donor/signup",dch.Authenticated(dch.Authorized(http.HandlerFunc(dch.DonorSignup))))
+	//http.HandleFunc("/donor/signup", dch.DonorSignup)
 	http.HandleFunc("/donor/login",dch.DonorLogin)
-	http.HandleFunc("/donors",dch.Doners)
+	//http.HandleFunc("/donors",dch.Doners)
 
 	http.HandleFunc("/recipient/signup",rch.RecipientSignup)
 	http.HandleFunc("/recipient/login",rch.RecipientLogin)
-	http.HandleFunc("/admin",rch.Recipients)
+	http.HandleFunc("/admin",ah.AdminLogin)
+	http.Handle("/admin/recipients",ah.Authenticated(ah.Authorized(http.HandlerFunc(rch.Recipients))))
+	http.Handle("/logout",rch.Authenticated(http.HandlerFunc(rch.Logout)))
 
-	http.HandleFunc("/recipientInfo",rich.CreateRecipientInfo)
-	http.HandleFunc("/category",rich.SelectApproved)
+	http.HandleFunc("/recipientInfo",rich.RecipientInfo)                           //Check this later
+	http.HandleFunc("/recipientInfo/create",rich.CreateRecipientInfo)
+	http.HandleFunc("/category",rich.ShowApproved)
+
+	http.HandleFunc("/individual/all",rich.SeeAll)
+	gd:="/"+handler.Path
+	http.HandleFunc(gd,rich.SeeIndividual)
 	http.HandleFunc("/approve",rich.ApproveRecipient)
 	http.HandleFunc("/admin/specific",rich.ViewSpecificRecipientInfo)
 
 
-	http.Handle("/logout", dch.Authenticated(http.HandlerFunc(dch.Logout)))
-	http.HandleFunc("/home",handler.Home)
+//	http.Handle("/logout", dch.Authenticated(http.HandlerFunc(dch.Logout)))
+	http.HandleFunc("/home",hch.Home)
 	http.HandleFunc("/category/medical",ch.SelectMedicalCategory)
 	http.HandleFunc("/category/women",ch.SelectWomenCategory)
-	http.HandleFunc("/category/eduacational",ch.SelectEducationalCategory)
+	http.HandleFunc("/category/eduacation",ch.SelectEducationalCategory)
 	http.HandleFunc("/category/orphanage",ch.SelectOrphanageCategory)
 	http.HandleFunc("/category/religious",ch.SelectReligiousCategory)
 	http.HandleFunc("/category/other",ch.SelectOtherCategory)
 	http.HandleFunc("/category/specific",ch.SelectSpecificCategory)
+//	http.Handle("category/donation",dch.Authenticated(dch.Authorized(http.HandlerFunc(ch.Donation))))
+	http.HandleFunc("/category/donation",ch.Donation)
+	http.HandleFunc("/money/transfer", ch.DonateMoney)
 
 
+	//for the REST API
+	http.HandleFunc("/v1/donors",dah.GetDonors)
+	http.HandleFunc("/v1/donor/",dah.GetDonor)
+	http.HandleFunc("/v1/donor",dah.PostDonor)
+	http.HandleFunc("/v1/donor/update/",dah.PutDonor)
+	http.HandleFunc("/v1/donor/delete/",dah.DeleteDonor)
+
+	http.HandleFunc("/v1/recipients",rah.GetRecipients)
+	http.HandleFunc("/v1/recipient/",rah.GetRecipient)
+	http.HandleFunc("/v1/recipient",rah.PostRecipient)
+	http.HandleFunc("/v1/recipient/update/",rah.PutRecipient)
+	http.HandleFunc("/v1/recipient/delete/",rah.DeleteRecipient)
+
+	http.HandleFunc("/v1/recipientsinfo",riah.GetRecipientsInfo)
+	http.HandleFunc("/v1/recipientinfo/",riah.GetRecipientInfo)
+	http.HandleFunc("/v1/recipientinfo",riah.PostRecipientInfo)
+	http.HandleFunc("/v1/recipientinfo/update/",riah.PutRecipientInfo)
+	http.HandleFunc("/v1/recipientinfo/delete/",riah.DeleteRecipientInfo)
 
 
-	router := httprouter.New()
-	router.GET("/v1/donor/:dlusername", dch.GetDonor)
-	router.GET("/v1/donors", dch.GetDonors)
-	router.POST("/v1/donor", dch.PostDonor)
 
 	http.ListenAndServe(":8080",nil)
 }
